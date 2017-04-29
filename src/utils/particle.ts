4ba5f01403ff9b5d52a6d1b1b3981d1ea009bb40
Thomas Sauer <t-sauer@outlook.de>
2017-04-29 20:41:19 +0200

    Create gh-pages branch

---
+++ src/utils/particle.ts
@@ -0,0 +1,50 @@
+export default class Particle {
+  position: { x: number, y: number };
+  velocity: { x: number, y: number };
+  radius: number;
+  lifeSpan: number;
+  inertia: number;
+  delete: boolean;
+
+  constructor(args: any) {
+    this.position = args.position;
+    this.velocity = args.velocity;
+    this.radius = args.size;
+    this.lifeSpan = args.lifeSpan;
+    this.inertia = 0.98;
+  }
+
+  destroy() {
+    this.delete = true;
+  }
+
+  render(state: any) {
+    // Move
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.x *= this.inertia;
+    this.velocity.y *= this.inertia;
+
+    // Shrink
+    this.radius -= 0.1;
+    if (this.radius < 0.1) {
+      this.radius = 0.1;
+    }
+    if (this.lifeSpan-- < 0) {
+      this.destroy();
+    }
+
+    // Draw
+    const context = state.context;
+    context.save();
+    context.translate(this.position.x, this.position.y);
+    context.fillStyle = '#ffffff';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(0, -this.radius);
+    context.arc(0, 0, this.radius, 0, 2 * Math.PI);
+    context.closePath();
+    context.fill();
+    context.restore();
+  }
+}
