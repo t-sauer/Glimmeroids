9e33315638db31a8ef8c1918ad1e2f39486d9dcd
Thomas Sauer <t-sauer@outlook.de>
2018-02-16 21:17:56 +0100

    Upgrade to Glimmer.js 0.9.1

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,11 @@
+'use strict';
+
 let browsers = [
   '> 5%',
   'last 2 Edge versions',
   'last 2 Chrome versions',
   'last 2 Firefox versions',
-  'last 2 Safari versions'
+  'last 2 Safari versions',
 ];
 
 if (process.env.EMBER_ENV === 'test') {
